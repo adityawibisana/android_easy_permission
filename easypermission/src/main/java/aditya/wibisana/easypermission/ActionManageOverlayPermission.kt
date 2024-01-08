74444aaca15e929366e88708f8834b6b745d5881
Aditya Wibisana <aditya.wibisana@gmail.com>
2024-01-08 20:34:45 +0700

    Add permission handling for draw over other app

--- easypermission/src/main/java/aditya/wibisana/easypermission/ActionManageOverlayPermission.kt
+++ easypermission/src/main/java/aditya/wibisana/easypermission/ActionManageOverlayPermission.kt
@@ -1,5 +1,54 @@
 package aditya.wibisana.easypermission
 
-class ActionManageOverlayPermission {
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
+class ActionManageOverlayPermission(appCompatActivity: AppCompatActivity)
+    : SimplePermission(appCompatActivity) {
+
+    override fun reload(activity: AppCompatActivity) {
+        _isPermissionGranted.value = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            true
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            Settings.canDrawOverlays(activity)
+        } else {
+            if (Settings.canDrawOverlays(activity)) true
+            try {
+                val mgr = activity.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                val viewToAdd = View(activity)
+                val params = WindowManager.LayoutParams(
+                    0,
+                    0,
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                    else
+                        WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSPARENT
+                )
+                viewToAdd.layoutParams = params
+                mgr.addView(viewToAdd, params)
+                mgr.removeView(viewToAdd)
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            false
+        }
+    }
+
+    override fun request(activity: AppCompatActivity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
+
+        val uri = Uri.parse("package:${activity.packageName}")
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri)
+        activity.startActivity(intent)
+    }
 }
